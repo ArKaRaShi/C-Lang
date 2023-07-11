e2415790fda6853491d0bb8257993f6e946edc3c
ArKaRaShi <rnwz12345@gmail.com>
2023-07-11 17:03:15 +0700

    update

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stddef.h>
 
 struct data {
     int firstNumber;
@@ -8,9 +9,12 @@ struct data {
 
 int main() {
     struct data newData;
-    printf("%llu bytes\n", sizeof (newData));
-    printf("%llu bytes\n", sizeof (newData.firstNumber));
-    printf("%llu bytes\n", sizeof (newData.secondNumber));
-    printf("%llu bytes\n", sizeof (newData.text));
+    printf("%p %zu bytes\n", &newData, sizeof(newData));
+    printf("%p %zu bytes\n", &newData.firstNumber, sizeof(newData.firstNumber));
+    printf("%zu bytes\n", sizeof(newData.secondNumber));
+    printf("%zu bytes\n", sizeof(newData.text));
+    printf("Offset of firstNumber: %zu\n", offsetof(struct data, firstNumber));
+    printf("Offset of secondNumber: %zu\n", offsetof(struct data, secondNumber));
+    printf("Offset of text: %zu\n", offsetof(struct data, text));
     return 0;
 }
