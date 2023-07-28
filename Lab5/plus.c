21dc7047b5d7094ada4b7c4913c4827a187d0553
ArKaRaShi <rnwz12345@gmail.com>
2023-07-28 15:38:37 +0700

    final plus

--- Lab5/plus.c
+++ Lab5/plus.c
@@ -1,52 +1,103 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-#define SIZE 50000
+char *mallocCharBySize(int size) {
+    return (char *) malloc(sizeof(char) * size);
+}
 
-int plusLargeNumber(char number1[], char number2[], char *ptrResultNumber) {
-    int smallNumberLength = strlen(number2);
-    int remainBoxes = strlen(number1) - strlen(number2);
-    int number1Index = strlen(number1) - 1, number2Index = strlen(number2) - 1, resIndex = strlen(number1) - 1;
-    *(ptrResultNumber + resIndex + 1) = '\0';
-    int sumOfTwoDigits, carry = 0;
-    for (int i = 0; i < smallNumberLength; i++) {
-        sumOfTwoDigits = number1[number1Index] - 48 + number2[number2Index] - 48 + carry;
-        carry = 0; 
-        if (sumOfTwoDigits >= 10) carry = 1;
-        *(ptrResultNumber + resIndex) = (char)(sumOfTwoDigits % 10 + 48); 
-        number1Index--;
-        number2Index--;
-        resIndex--;
+char *reallocCharBySize(char *ptr, int size) {
+    return (char *) realloc(ptr, sizeof(char) * size);
+}
+
+int getInput(char **ptrArr) {
+    int countSize = 1;
+    while (1) {
+        char temp = getchar();
+        if (temp == '\n') 
+            break;
+
+        *(*ptrArr + (countSize - 1)) = temp;
+        *ptrArr = reallocCharBySize(*ptrArr, sizeof(char) * (++countSize));
+        *(*ptrArr + (countSize - 1)) = '\0';
     }
+    return countSize;
+}
 
-    for (int i = 0; i < remainBoxes; i++) {
-        if (carry) {
-            *(ptrResultNumber + i) = (number1[i] - 48 + carry) % 10 + 48;
-            if (number1[i] - 48 + carry < 10) carry = 0; 
-        }
-        else
-            *(ptrResultNumber + i) = number1[i];
-    }   
+int copyRemainValue(char **pResultArr, char *ptrArr, int pointIndex, int carry) {
+    int copyValue;
+    int repeat = pointIndex + 1;
+    for (int i = 0; i < repeat; i++) {
+        copyValue = *(ptrArr + pointIndex) - 48 + carry;
 
-    if (carry) return 1;
-    return 0;
+        if (copyValue >= 10) 
+            carry = 1;
+        else 
+            carry = 0;
+
+        *(*pResultArr + pointIndex) = copyValue % 10 + 48;
+
+        pointIndex--;
+    }
+    return carry;
+}
+
+int plus(char **pResultArr, char *pNumArr1, char *pNumArr2, int sizeArr1, int sizeArr2) {
+    int pointIndex1 = sizeArr1 - 2, pointIndex2 = sizeArr2 - 2, pointResultIndex; 
+    int repeat;
+    int carry, largerArr;
+    if (sizeArr1 > sizeArr2) {
+        *pResultArr = reallocCharBySize(*pResultArr, sizeArr1);
+        pointResultIndex = sizeArr1 - 2;
+        repeat = sizeArr2 - 1;
+        largerArr = 1;
+    }
+    else {
+        *pResultArr = reallocCharBySize(*pResultArr, sizeArr2);
+        pointResultIndex = sizeArr2 - 2;
+        repeat = sizeArr1 - 1;
+        largerArr = 0;
+    }
+    *(*pResultArr + pointResultIndex + 1) = '\0';
+
+    carry = 0;
+    for (int i = 0; i < repeat; i++) {
+        int sumOfTwoDigits = (*(pNumArr1 + pointIndex1) - 48) + (*(pNumArr2 + pointIndex2) - 48) + carry;
+        if (sumOfTwoDigits >= 10) 
+            carry = 1;
+        else 
+            carry = 0;
+
+        *(*pResultArr + pointResultIndex) = (char)(sumOfTwoDigits % 10 + 48);
+
+        pointIndex1--;
+        pointIndex2--;
+        pointResultIndex--;
+    }
+    if (largerArr) 
+        carry = copyRemainValue(&*pResultArr, pNumArr1, pointIndex1, carry);
+    else 
+        carry = copyRemainValue(&*pResultArr, pNumArr2, pointIndex2, carry);
+    return carry;
+}
+
+void displayResult(char *ptrArr, int carry) {
+    if (carry) 
+        printf("1%s", ptrArr);
+    else
+        printf("%s", ptrArr);
 }
 
 int main(void) {
-    char numberS1[SIZE], numberS2[SIZE], resultNumber[SIZE + 1];
+    char *pNumArr1 = mallocCharBySize(1);
+    char *pNumArr2 = mallocCharBySize(1);
+    char *pResultArr = mallocCharBySize(1);
+    int sizeArr1 = getInput(&pNumArr1);
+    int sizeArr2 = getInput(&pNumArr2);
     int carry;
-
-    scanf("%s\n", numberS1);
-    scanf("%s", numberS2);
-    if (strlen(numberS1) > strlen(numberS2))
-        carry = plusLargeNumber(numberS1, numberS2, resultNumber);
-    else
-        carry = plusLargeNumber(numberS2, numberS1, resultNumber);
-
-    if (carry)
-        printf("1%s", resultNumber);
-    else    
-        printf("%s", resultNumber);
+    carry = plus(&pResultArr, pNumArr1, pNumArr2, sizeArr1, sizeArr2);
+    displayResult(pResultArr, carry);
+    free(pNumArr1);
+    free(pNumArr2);
+    free(pResultArr);
     return 0;
 }
